9ff03adf191163ea1134f21f6897647b9eefe3c0
Prabhalaa2 <30726851+Prabhalaa2@users.noreply.github.com>
2017-08-17 11:31:33 -0400

    Loginfourm

---
+++ LogInFourm/LogInFourm/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As String = Userbox.Text
+        Dim y As String = PassBox.Text
+        If x = ("CCLC") And y = ("Summer17") Then
+            MsgBox("Welcome to the Matrix")
+        Else
+            MsgBox("You are incorrect")
+        End If
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim webadress As String = "//www.google.com/doodles/"
+        Process.Start(webadress)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
